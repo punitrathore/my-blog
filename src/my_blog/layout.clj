dc9e11b063590524bf73ee23724b5294bbc28097
Punit Rathore <punitr@gmail.com>
2010-12-28 15:37:42 +0530

    adding yui layout

--- src/my_blog/layout.clj
+++ src/my_blog/layout.clj
@@ -1,6 +1,6 @@
 (ns my-blog.layout
   (:use (hiccup [core :only [html escape-html]]
-                [page-helpers :only [link-to]]
+                [page-helpers :only [link-to include-css include-js]]
                 [form-helpers :only [form-to text-field text-area hidden-field]])))
 
 (defn wrap-with-layout [page-html]
@@ -8,11 +8,25 @@
    [:html
     [:head
      [:title "Punit Rathore's Blog"]
-     [:link {:type "application/rss+xml" :rel "alternate" :href "/feed"}]]
+     [:link {:type "application/rss+xml" :rel "alternate" :href "/feed"}]
+     (include-css "/css/yui/base-min.css")
+     (include-css "/css/yui/reset-min.css")
+     (include-css "/css/yui/fonts-min.css")
+     (include-css "/css/yui/grids-min.css")     
+     (include-css "/css/common.css")]
     [:body
-     [:div#header
+     [:div#hd
       [:h1 "Welcome to Punit's blog!"]
        [:span.header-link (link-to "/posts" "Home")]
       [:span.header-link (link-to "/posts/new" "New Post")]]
-     [:div#content
-      page-html]]]))
+     [:div#doc4.yui-t4
+      [:div#bd
+       
+       [:div#main-page.yui-gf
+        [:div#content-container.yui-u        
+        [:div#content
+         page-html]]
+       [:div#sidebar.yui-u.first
+        "Sidebar"]]]
+      ]
+     ]]))
--- src/my_blog/routes.clj
+++ src/my_blog/routes.clj
@@ -1,6 +1,7 @@
 (ns my-blog.routes
-  (:use [compojure.core])
-  (:require [compojure.route :as route]
+  (:use (compojure.core)
+        )
+  (:require [compojure.route :as route]           
             [my-blog.post :as post]
             [clj-json.core :as json]
             [my-blog.html :as html]
@@ -13,10 +14,12 @@
    :body (json/generate-string data)})
 
 (defroutes post-routes
+  (GET "/" [] (views/index-page))
   (GET "/posts" [] (views/index-page))
   (GET "/posts/new" [] (views/new-post-page))
   (GET "/posts/:id" [id] (layout/wrap-with-layout (html/render-full-post (post/find-post id))))
   (POST "/posts" {params :params} (views/create-new-post params))
   (POST "/comments" {params :params} (views/create-new-comment params))
   (route/files "/" {:root "public"})
-  (route/not-found "Page not found"))
+  (route/not-found "Page not found")
+  )
